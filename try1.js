c6386c88992ef35174d623686049b5c402fe35b5
tyler-bair <52551917+tyler-bair@users.noreply.github.com>
2023-01-10 21:35:11 -0700

    Create try1.js

---
+++ try1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, } from 'reactstrap';
+import { memberData, teamData } from "./teamInfo";
+import AboutCard from "./AboutCard";
+
+export default function About() {
+    return (
+        <Container>
+            Hello World
+        </Container>
+    );
+}
